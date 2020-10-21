c130f01d94f7cfc8d5d639b4d18d02c041af9126
zahitkaya <zahit_kaya58@hotmail.com>
2020-10-21 15:54:23 +0300

    Small changes

--- src/main/java/com/example/demo/solid/dependency_inversion/IControll.java
+++ src/main/java/com/example/demo/solid/dependency_inversion/IControll.java
@@ -1,5 +1,9 @@
 package com.example.demo.solid.dependency_inversion;
-
+/*
+Varlıklar(Alt sınıflar ve Üst sınıflar) somut olmayan soyutlamalara bağlı olmalıdır. Üst seviye modülün düşük seviye
+modülüne bağlı olmamasını, ancak soyutlamalara bağlı olması gerektiğini belirtir. Alt sınıflarda yapılan
+ değişiklikler üst sınıfları etkilememelidir.
+ */
 public interface IControll {
     void apply();
 }
